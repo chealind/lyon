254e247e96bf59b87c758ed64ec386c41f825728
Bohdan Bachkala <bbachkala@proton.me>
2024-12-04 07:27:41 +0200

    add terraform

---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  cloud {
+    organization = "bbachkala"
+
+    workspaces {
+      name = "lyon"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+module "ami-ubuntu" {
+  source  = "andreswebs/ami-ubuntu/aws"
+  version = "1.1.0"
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.5.0"
+  count   = 1
+
+  name = "ec2-nginx"
+
+  ami           = module.ami-ubuntu.ami_id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "nginx_sg" {
+  name   = "nginx_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
