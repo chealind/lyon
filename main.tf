8ed044156ceb1e0d25ce203870cc9e02d0cb7dd2
Bohdan Bachkala <bbachkala@proton.me>
2024-12-04 07:34:23 +0200

    update terraform format

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
@@ -33,8 +33,8 @@ module "ec2_instances" {
 
   name = "ec2-nginx"
 
-  ami           = module.ami-ubuntu.ami_id
-  instance_type = "t2.micro"
+  ami                    = module.ami-ubuntu.ami_id
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 }
 
@@ -45,23 +45,23 @@ resource "aws_security_group" "nginx_sg" {
   vpc_id = aws_default_vpc.default.id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
